645112327b8650c5abedb8064ecdff6c4109b289
Marta <turkovska05@gmail.com>
2022-12-23 00:32:44 +0200

    slayyyy

---
+++ UnitTest8.1iter/UnitTest8.1iter.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1iter/lab_08.1iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81iter
+{
+	TEST_CLASS(UnitTest81iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char cs[] = "+-=";
+			int m = Count(cs);
+			Assert::AreEqual(m, 2);
+		}
+	};
+}
